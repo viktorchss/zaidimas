548d61ea78c87fb3213f32baaef5af54bbb6b276
MACKUNAS <58569@0008554401-77>
2024-11-07 11:00:54 +0200

    zaidimas

---
+++ program/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    int v = m - n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (j < m - v - i || j > m - i)
+                cout << "🌍";
+            else
+                cout << "🧱";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
